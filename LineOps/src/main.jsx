f6f27e449a02fb7b77002b720759fba0f7205456
R-Z-W <whiteleyzenki@gmail.com>
2024-12-27 20:59:34 +1100

    Initial Setup (cont.)

--- LineOps/src/main.jsx
+++ LineOps/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Development error reporting
+if (import.meta.env.DEV) {
+  console.log('Running in development mode');
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
+
+// Enable HMR for development using Vite's API
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
